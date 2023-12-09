2327e69ce086d2a7c65983000372a47b38247012
KekiGC <eduardoeg002@gmail.com>
2023-12-09 09:01:07 -0400

    Reviews and comments

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,25 +1,29 @@
 import { Router } from 'express';
 const router = Router();
 
-import { getMovies, getMovie, deleteMovie, addPublicRating, addCriticRating,
+import { getMovies, getMovie, deleteMovie, addRating,
 getMoviesByGenre, getActors, getTopRatedMovies, getMoviesByDuration,
-addComment, getMovieByTitle } from '../controllers/movie.controller';
+addComment, getComments, getMovieByTitle, getLatestMovies, getMovieReviews } from '../controllers/movie.controller';
 
 // Routes
 // Gets
 router.get('/movies', getMovies)
 router.get('/movies/:movieId', getMovie)
+router.get('/movies/search/latest', getLatestMovies)
 router.get('/movies/genre/:genre', getMoviesByGenre)
 router.get('/movies/duration/:duration', getMoviesByDuration)
 router.get('/movies/rating/toprated', getTopRatedMovies)
 router.get('/movies/search/:title', getMovieByTitle)
+router.get('/movies/:movieId/reviews', getMovieReviews)
 router.get('/actors', getActors)
 router.get('/movie/:title', getMovieByTitle);
+router.get('/movies/:movieId/comments', getComments)
 
 // Posts
 router.post('/movies/:movieId/comment', addComment)
-router.post('/movies/:movieId/rating/public', addPublicRating)
-router.post('/movies/:movieId/rating/critic', addCriticRating)
+router.post('/movies/:movieId/rating', addRating)
+// router.post('/movies/:movieId/rating/public', addPublicRating)
+// router.post('/movies/:movieId/rating/critic', addCriticRating)
 
 router.delete('/movies/:movieId', deleteMovie)
 
